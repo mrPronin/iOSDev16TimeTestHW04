ae2f9a2f9dc4b79c28e66f147b9ec5ed1ae8475e
Alexander Pronin <pronin.alx@gmail.com>
2014-02-19 23:02:42 +0200

    Initial Commit

---
+++ 16TimeTestHW04Tests/_6TimeTestHW04Tests.m
@@ -0,0 +1,34 @@
+//
+//  _6TimeTestHW04Tests.m
+//  16TimeTestHW04Tests
+//
+//  Created by Pronin Alexander on 19.02.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _6TimeTestHW04Tests : XCTestCase
+
+@end
+
+@implementation _6TimeTestHW04Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
